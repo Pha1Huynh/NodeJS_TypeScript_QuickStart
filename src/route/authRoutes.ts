b8da02fcc0602e163ac78793be5116303864fc10
Kevin Tan <phat.huynhtan@ncc.asia>
2023-03-06 10:07:31 +0700

    optimize

--- src/route/authRoutes.ts
+++ src/route/authRoutes.ts
@@ -4,10 +4,5 @@ import authController from '../controllers/authController'
 import authMiddleware from "../middleware/authMiddleware";
 
 let router = express.Router();
-
-
 router.post('/refresh-token',authMiddleware.authenToken,authController.refreshAccessToken )
-  
- 
-
 export = router;
